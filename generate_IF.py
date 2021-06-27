85d86bd38aca3ece16a3cb3278d980fa051cebc6
Eason Ding <66626734+Ding-Papa@users.noreply.github.com>
2021-06-27 21:42:34 +0800

    Update generate_IF.py
    optimization of code

--- generate_IF.py
+++ generate_IF.py
@@ -10,8 +10,8 @@ class Inverted_File(MRJob):
     def mapper(self, _, line):
         temp = line.split('\t')
         te = temp[0].split(' ')
-        title = te[0]  #　获取题目
-        url = te[1]
+        title = ''.join(te[:-1])  # 获取题目,防止题目中本身有空格
+        url = te[-1]
         text = temp[1]  # 获取正文内容
         words = text.split(' ')  # 预先已经进行过jieba分词，此处生成包含各词的list
         count = Counter(words)  # 用collections包的Counter方法来进行词频统计，高效且方便
@@ -25,7 +25,7 @@ class Inverted_File(MRJob):
         val = temp.split('\t')    # 将同一个词语对应的"题目+url+tf"字串转化到一个列表内
         ls = []
         count = len(val)  # 统计包含当前词语的文档数
-        total_title = 300   # total number of documents
+        total_title = 2178500   # total number of documents
         for ele in val:
             temp2 = ele.split('  ')
             tf_idf = eval(temp2[-1]) * math.log(total_title * 1.0 / count + 1)  # 计算tf-idf值
